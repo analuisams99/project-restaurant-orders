13c77aeb96ff452f657106a9b225af2ff7279141
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-08-24 21:22:38 -0300

    Implementa a classe TrackOrders que gera informações contínuas da lanchonete

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,65 @@
+from collections import Counter
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque.
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        customer_orders = [
+            order[1] for order in self.orders if order[0] == customer
+        ]
+        result = Counter(customer_orders).most_common()[0][0]
+
+        return result
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        customer_orders = set()
+        all_orders = set()
+
+        for customer_name, order, _ in self.orders:
+            all_orders.add(order)
+            if customer_name == customer:
+                customer_orders.add(order)
+
+        return all_orders.difference(customer_orders)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        customer_days = set()
+        all_days = set()
+
+        for customer_name, _, day in self.orders:
+            all_days.add(day)
+            if customer_name == customer:
+                customer_days.add(day)
+
+        return all_days.difference(customer_days)
 
     def get_busiest_day(self):
-        pass
+        busiest_day = []
+
+        for _, _, day in self.orders:
+            busiest_day.append(day)
+
+        return Counter(busiest_day).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        least_busy_day = []
+
+        for _, _, day in self.orders:
+            least_busy_day.append(day)
+
+        return Counter(least_busy_day).most_common()[-1][0]
+
+    def get_customer_meal_count(self, customer, meal):
+        customer_orders = [
+            order[1] for order in self.orders if order[0] == customer
+        ]
+        meal_counter = [order for order in customer_orders if order == meal]
+        return len(meal_counter)
